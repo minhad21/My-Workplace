3948f81f153716db5193d57a5a99e95ef32b38b3
minhad21 <mdemonkorim@gmail.com>
2025-12-09 17:23:31 +0600

    script

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-// Prompt er kaj
-let emon= prompt('Enter Your Name');
+// // Prompt er kaj
+// let emon= prompt('Enter Your Name');
 
-console.log(emon);
+// console.log(emon);
 
-// Alert er kaj
-alert("Oh no this is emon's ex");
+// // Alert er kaj
+// alert("Oh no this is emon's ex");
 
-// geneate random number
+// // geneate random number
+let x=Math.trunc((Math.random()*20))+1;
+console.log(x);
 
+//bmi calculation
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 95;
+const heightJohn = 1.88;
+
+ const BMIMark = massMark/(heightMark*heightMark);
+ const BMIJohn = massJohn/(heightJohn*heightJohn);
+
+ console.log(BMIMark);
+ console.log(BMIJohn);
